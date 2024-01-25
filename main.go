9fdd5b071ece627fb8000adf97f2ae0508b46c8c
daniel-z-johnson <danzaljohnson@gmail.com>
2024-01-25 17:59:20 -0600

    Stopping point, logic for getting locations key, some logic for sqlite3

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"github.com/daniel-z-johnson/peronalWeatherSite/config"
-	"github.com/daniel-z-johnson/peronalWeatherSite/models"
+	"context"
+	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitemigration"
+
 	"log/slog"
 	"os"
 )
@@ -12,14 +14,50 @@ func main() {
 	logger = logger.With("application", "personal weather application")
 	logger.Info("Application start")
 
+	conn, err := connectDB(logger)
+	if err != nil {
+		// nothing can be done so give up and cause program to crash
+		panic(err)
+	}
+	migrate(conn, logger)
+
 	// quick testing delete later
-	fileLoc := "config.json"
-	conf, err := config.LoadConfig(&fileLoc)
+	// fileLoc := "config.json"
+	//conf, err := config.LoadConfig(&fileLoc)
+	//if err != nil {
+	//	logger.Error(err.Error())
+	//	panic(err)
+	//}
+	// wa := models.WeatherService(logger, conf)
+	// geoPoints, err := wa.GetGeoPoints()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//for _, geo := range geoPoints {
+	//	fmt.Printf("%+v\n", geo)
+	//}
+}
+
+func connectDB(logger *slog.Logger) (*sqlite.Conn, error) {
+	conn, err := sqlite.OpenConn("w.db", sqlite.OpenReadWrite, sqlite.OpenCreate)
 	if err != nil {
 		logger.Error(err.Error())
 		panic(err)
 	}
-	wa := models.WeatherService(logger, conf)
-	wa.GetGeoPoints()
+	return conn, nil
+}
 
+func migrate(conn *sqlite.Conn, logger *slog.Logger) {
+	schema := sqlitemigration.Schema{
+		AppID: 0xb19b66b,
+		Migrations: []string{
+			"CREATE TABLE locations(id INTEGER PRIMARY KEY AUTOINCREMENT, postal_code text, country text, key text, timestamp datetime, UNIQUE(postal_code, country))",
+		},
+	}
+	err := sqlitemigration.Migrate(context.Background(), conn, schema)
+	if err != nil {
+		logger.Error(err.Error())
+		panic(err)
+	}
+	logger.Info("Migration Success")
 }
--- models/weather.go
+++ models/weather.go
@@ -1,9 +1,11 @@
 package models
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/daniel-z-johnson/peronalWeatherSite/config"
 	"log/slog"
+	"net/http"
 	"net/url"
 )
 
@@ -27,8 +29,8 @@ func WeatherService(logger *slog.Logger, config *config.Config) *WeatherAPI {
 	return &WeatherAPI{logger: logger, config: config}
 }
 
-func (wa *WeatherAPI) GetGeoPoints() ([]GeoLocation, error) {
-	geoPoints := make([]GeoLocation, 0)
+func (wa *WeatherAPI) GetGeoPoints() ([]*GeoLocation, error) {
+	geoPoints := make([]*GeoLocation, 0)
 	for _, zip := range wa.config.Zipcodes {
 		u, err := url.Parse("https://dataservice.accuweather.com/locations/v1/postalcodes/search")
 		if zip.CountryCode != "" {
@@ -39,11 +41,21 @@ func (wa *WeatherAPI) GetGeoPoints() ([]GeoLocation, error) {
 			return nil, err
 		}
 		values := u.Query()
-		values.Set("zip", zip.PostalCode)
+		values.Set("q", zip.PostalCode)
 		values.Set("apikey", wa.config.WeatherAPI.Key)
 		u.RawQuery = values.Encode()
 		wa.logger.Info(u.String())
+		geoLoc := make([]*GeoLocation, 0)
+		r, err := http.Get(u.String())
+		if err != nil {
+			return nil, err
+		}
+		defer r.Body.Close()
+		err = json.NewDecoder(r.Body).Decode(&geoLoc)
+		if err != nil {
+			return nil, err
+		}
+		geoPoints = append(geoPoints, geoLoc[0])
 	}
-
 	return geoPoints, nil
 }
